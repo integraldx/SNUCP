55b1e04e55c58f63ef3f4270f68f0fe242186429
integraldx <jmh121463@gmail.com>
2018-10-18 13:46:54 +0900

    Testor for toCharArray and equals

--- HW#2/StringTestor.java
+++ HW#2/StringTestor.java
@@ -1,8 +1,47 @@
 
 class StringTestor{
     public static void main(String[] args){
-        MyString myString = new MyString("Hello World!");
+        if(examineMyString("Hello World!")){
+            System.out.println("test passed!");
+            
+        }
+        else {
+            System.out.println("test failed.");
+        }
 
-        System.out.println(myString.toCharArray());
+        
+    }
+    
+    public static boolean examineMyString(String input){
+        MyString myString = new MyString(input.toCharArray());
+        boolean passed = true;
+        System.out.println("Input String : " + input + "\n");
+
+        System.out.println("toCharArray() test");
+        System.out.println("String : " + new String(input.toCharArray()));
+        System.out.println("MyString: " + new String(myString.toCharArray()));
+        System.out.println("");
+        for(int i = 0; i < input.length(); i++){
+            if(input.toCharArray()[i] != myString.toCharArray()[i]){
+                passed = false;
+            }
+        }
+
+        System.out.println("equals() test");
+        System.out.println("String(random String) : " + input.equals("klaj;sdfjklfds afjsdk"));
+        System.out.println("MyString(random String) : " + myString.equals(new MyString("klaj;sdfjklfds afjsdk")));
+        if(input.equals("klaj;sdfjklfds afjsdk") != myString.equals(new MyString("klaj;sdfjklfds afjsdk"))){
+            passed = false;
+        }
+        System.out.println("String(equal String) : " + input.equals(new String(input.toCharArray())));
+        System.out.println("MyString(equal String) : " + myString.equals(new MyString(input.toCharArray())));
+        if(input.equals(new String(input.toCharArray())) != myString.equals(new MyString(input.toCharArray()))){
+            passed = false;
+        }
+        System.out.println("");
+
+        
+
+        return passed;
     }
 }
