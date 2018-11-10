999d1cf5c5c3179ba0bc38f8d3a00b93bab79a40
integraldx <jmh121463@gmail.com>
2018-11-10 16:56:25 +0900

    HW#3 prepare

---
+++ HW#3/LBST_Node.java
@@ -0,0 +1,7 @@
+
+class LBST_Node {
+    public int val;
+    public LBST_Node l_child;
+    public LBST_Node r_child;
+    public LBST_Node next;
+}
---
+++ HW#3/LinkedBinarySearchTree.java
@@ -0,0 +1,25 @@
+
+class LinkedBinarySearchTree {
+    public LBST_Node root;
+    public LBST_Node first;
+
+    public void insert(int num) {
+
+    }
+
+    public void remove(int num) {
+
+    }
+
+    public boolean search(int num) {
+
+    }
+
+    public boolean range_search(int left_val, int right_val, int num) {
+
+    }
+
+    public LBST_Node[] list() {
+        
+    }
+}
