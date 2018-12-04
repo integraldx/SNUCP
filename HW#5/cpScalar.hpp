257949ee852919a3d37a42474002f5bcdea45597
integraldx <jmh121463@gmail.com>
2018-12-04 14:52:18 +0900

    idk if this works

--- HW#5/cpScalar.hpp
+++ HW#5/cpScalar.hpp
@@ -5,8 +5,14 @@ public:
     cpScalar(int num);
     cpScalar(double num);
 
+    cpScalar operator +(cpScalar scalar);
+    cpScalar operator -(cpScalar scalar);
+    cpScalar operator *(cpScalar scalar);
+    cpScalar operator /(cpScalar scalar);
+    operator int();
+    operator double();
+
 private:
-    int value;
     double value;
 
 };
@@ -17,4 +23,28 @@ cpScalar::cpScalar(int num) {
 
 cpScalar::cpScalar(double num) {
     this->value = num;
+}
+
+cpScalar cpScalar::operator +(cpScalar scalar) {
+    cpScalar toReturn(value + scalar.value);
+    return toReturn;
+} 
+
+cpScalar cpScalar::operator -(cpScalar scalar) {
+    cpScalar toReturn(value - scalar.value);
+    return toReturn;
+}
+
+cpScalar cpScalar::operator *(cpScalar scalar) {
+    cpScalar toReturn(value * scalar.value);
+    return toReturn;
+}
+
+cpScalar cpScalar::operator /(cpScalar scalar) {
+    cpScalar toReturn(value / scalar.value);
+    return toReturn;
+}
+
+cpScalar::operator int() {
+    return value;
 }
