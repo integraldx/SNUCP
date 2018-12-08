7db8eea212b8b967d926a4827c7b6405de4ce5f5
integraldx <jmh121463@gmail.com>
2018-12-09 05:44:27 +0900

    no solution & 3 answer limit implementation

--- HW#5/numberQuiz.cpp
+++ HW#5/numberQuiz.cpp
@@ -8,12 +8,22 @@ int main() {
     getline(cin, str);
 
     StringNumIterator iter(str);
-    
+
+    int answerCount = 0;
+
     while(!iter.isEnd()) {
+        if(answerCount == 3) {
+            break;
+        }
         EvalTree evalTree(iter.getNext());
         if(evalTree.evaluate()) {
             cout << iter.getBoxNumString() << endl;
+            answerCount++;
         }
+    }
+
+    if(answerCount == 0) {
+        cout << "No solution" << endl;
     }
 
 
