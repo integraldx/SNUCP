ae444624737b8a3504902f196d7dc6ed6b6d2cc2
integraldx <jmh121463@gmail.com>
2018-12-09 04:12:09 +0900

    stringIterator implementation

---
+++ HW#5/StringNumIterator.hpp
@@ -0,0 +1,119 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <memory>
+#include "Node.hpp"
+
+using namespace std;
+
+class StringNumIterator
+{
+  public:
+    StringNumIterator(string str);
+    ~StringNumIterator();
+    string getNext();
+    bool isEnd();
+    int *getBoxNums();
+
+  private:
+    string orignalString;
+    int boxCount = 0;
+    char *boxes;
+    long long int iterationCount = 0;
+
+};
+
+StringNumIterator::StringNumIterator(string str)
+{
+    orignalString = str;
+
+    for (char ch : str)
+    {
+        if (ch == '[')
+        {
+            boxCount++;
+        }
+    }
+
+    boxes = (char *)calloc(boxCount, sizeof(char));
+
+    for (int i = 0; i < boxCount; i++)
+    {
+        boxes[i] = 0;
+    }
+
+    cout << boxCount << endl;
+}
+
+StringNumIterator::~StringNumIterator() {
+    delete boxes;
+}
+
+string StringNumIterator::getNext()
+{
+    if (iterationCount != 0)
+    {
+        bool carry = false;
+        boxes[boxCount - 1]++;
+
+        if (boxes[boxCount - 1] >= 10)
+        {
+            carry = true;
+            boxes[boxCount - 1] %= 10;
+        }
+        for (int i = boxCount - 2; i >= 0; i--)
+        {
+            if (carry)
+            {
+                boxes[i]++;
+                carry = false;
+            }
+
+            if (boxes[i] >= 10)
+            {
+                carry = true;
+                boxes[i] = boxes[i] % 10;
+            }
+
+        }
+    }
+
+    string toReturn;
+    bool box = false;
+    int i = 0;
+    for (char ch : orignalString)
+    {
+        if (box)
+        {
+            box = false;
+            continue;
+        }
+
+        if (ch == '[')
+        {
+            toReturn.push_back(boxes[i] + '0');
+            box = true;
+            i++;
+        }
+        else
+        {
+            toReturn.push_back(ch);
+        }
+    }
+
+    iterationCount++;
+    return toReturn;
+}
+
+bool StringNumIterator::isEnd()
+{
+    if (iterationCount == 100)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
--- HW#5/numberQuiz.cpp
+++ HW#5/numberQuiz.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
-#include <memory>
 #include "EvalTree.hpp"
+#include "StringNumIterator.hpp"
 
 using namespace std;
 int main() {
     string str;
     getline(cin, str);
-    EvalTree* evalTree = new EvalTree(str);
 
-    cout << evalTree->evaluate() << endl;
+    StringNumIterator iter(str);
+    while(!iter.isEnd()) {
+        cout << iter.getNext() << endl;
+    }
+
+    // EvalTree* evalTree = new EvalTree(str);
+
+    // cout << evalTree->evaluate() << endl;
 
     return 0;
 }
